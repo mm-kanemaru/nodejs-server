c82b43ed42bb89bf3c4a595449c6ee8105c85fcf
kanemaru <kanemaru@macromill.com>
2025-07-17 17:30:05 +0900

    reflect fb

--- app.js
+++ app.js
@@ -4,38 +4,43 @@ const port = 2000;
 
 // launch the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
 // returns {"message": "Hello, world!"} as JSON
 app.get('/', (req, res) => {
-  res.json({"message": "Hello, world!"})
-})
+  res.json({message: 'Hello, world!'});
+});
 
 // returns [{"name": "name"}, ...] that includes FE Sharing members as JSON
 const feMemberList = [
-  {"name": "Rayyan"},
-  {"name": "Shibu"},
-  {"name": "Sensui"},
-  {"name": "Ram"},
-  {"name": "Atlin"},
-  {"name": "Rio"}
-]
+  {name: 'Rayyan'},
+  {name: 'Shibu'},
+  {name: 'Sensui'},
+  {name: 'Ram'},
+  {name: 'Atlin'},
+  {name: 'Rio'}
+];
 
 app.get('/users', (req, res) => {
-  res.json(feMemberList)
-})
+  res.json(feMemberList);
+});
 
 // accepts {"name": <string>} that is an user to add as JSON
 // returns {"name": <added user name>} as JSON
+
+// parse the request body as JSON
+app.use(express.json());
+
 app.post('/users', (req, res) => {
-  const { name } = req.body;
+  const {name} = req.body;
   
   if (!name || typeof name !== 'string') {
     return res.status(400).json({"error": "Name is required and must be a string"});
   }
+
   // Return the added user name
-  res.json({"name": name});
-})
+  res.json({name: name});
+});
 
 
